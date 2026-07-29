6addbb9c931ff6fe904b9248f738dc7e5c2d4670
Sarif-pixel <sarifsekh96355@gmail.com>
2026-07-30 00:18:31 +0530

    assign comp

---
+++ 01 Learning Phase/03 Linear Search/linearsearch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, key, i, found = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            found = 1;
+            break;      // Exit the loop when the element is found
+        }
+    }
+
+    if(found == 1)
+        printf("Element found at position %d", i + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
--- 01 Learning Phase/05 Bubble Sort/bubblesort.c
+++ 01 Learning Phase/05 Bubble Sort/bubblesort.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
-void main (){
-    
+
+int main(){
+    int arr[100],n,i,j,temp;
+
+    printf("Eneter number of element: ");
+    scanf("%d", &n);
+
+    printf("Enter the element:\n");
+    for (i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    // Bubble Sort
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+    printf("Sorted Array:\n");
+    for(i=0;i<n;i++){
+        printf("%d  ",arr[i]);
+    }
+    return 0;
+
 }
